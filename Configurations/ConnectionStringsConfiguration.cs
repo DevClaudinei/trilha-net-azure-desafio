0a814727bfb60fa5e0d579ac385d6060f818c578
Claudinei José Santos <claudineibitta@gmail.com>
2023-11-20 18:02:19 -0300

    feat: criacao de configuracoes de banco

---
+++ Configurations/ConnectionStringsConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using DomainModels.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace trilha_net_azure_desafio.Configurations
+{
+    public static class ConnectionStringsConfiguration
+    {
+        public static void AddConnectionStringsConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+           services.Configure<ConnectionStrings>(configuration.GetSection("ConnectionStrings"));    
+        }
+    }
+}
---
+++ Configurations/DbConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using EntityFrameworkCore.UnitOfWork.Extensions;
+using Infraestructure.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace trilha_net_azure_desafio.Configurations
+{
+    public static class DbConfiguration
+    {
+        public static void AddDbConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<RHContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("ConexaoPadrao"),
+                                b => b.MigrationsAssembly("Infraesctructure.Data"));
+            }, contextLifetime: ServiceLifetime.Transient);
+            services.AddUnitOfWork<RHContext>(ServiceLifetime.Transient);
+        }
+    }
+}
